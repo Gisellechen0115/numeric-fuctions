09caa00a03834be523ca50c491df33ea40adf1d8
GISELLE CHEN <makitru0115@gmail.com>
2021-07-01 21:56:51 +0800

    first commit

---
+++ round_floats.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun 21 00:59:52 2021
+
+@author: ASUS
+"""
+
+#1.235  
+a = 1.23456789
+print(round (a , 3))
+print(round ( a ))
+#ceil()
+#floor(a))
+b=(round(a , 3))
+print(b)
+
