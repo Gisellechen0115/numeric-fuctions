d42ebd4386f7af851000e0555a422c19fe6b4c9d
GISELLE CHEN <makitru0115@gmail.com>
2021-07-01 21:54:49 +0800

    first commit

---
+++ exponentiation_Quotient.py
@@ -0,0 +1,27 @@
+#exponentiation 
+#32 , 16 , 3.0
+print( 2**5 )  #output 2 raised to the 5th power
+
+#practice
+a=1000000
+p = 0.01*(2**30)
+if p > a:
+    print('選方案二,多了',p-a,'元')
+else:
+    print('選方案一',a,'元')
+
+print(2**2**2)
+
+print( 9** (1/2) ) #the square root of 9, the result will be a float
+print( 8**(1/3) ) #8**(1/3)=(2*3)**(1/3)=2**(3*1/3)
+
+#Quotient商數
+print( 20//6 ) #20//6=3 int #output the quotient of dividing 20 by 6.
+print(20.0//6) # 20.0//6=3.0 flaot
+print(20/6)  #20/6=3.33333333  float
+#remainder餘數 that is used to get the remainder od a di vision
+print( 20 % 6 ) #20%6=2 
+print( 20.0 % 6)# 20.02%6=2.0
+
+print( 7 % (5//2)) #7%(5//2)=7%2=1
+
